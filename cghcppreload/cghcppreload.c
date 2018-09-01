2a697796a5d8f9874ba1c087d0cd4d682550b2fd
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-09-01 22:45:07 +0300

    Override getaddrinfo

--- cghcppreload/cghcppreload.c
+++ cghcppreload/cghcppreload.c
@@ -35,6 +35,147 @@ int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
   return connect_ex_dst(sockfd, &resolvs[idx], ntohs(sin->sin_port));
 }
 
+void freeaddrinfo(struct addrinfo *res)
+{
+  if (res->ai_next != NULL)
+  {
+    abort();
+  }
+  free(res->ai_addr);
+  free(res);
+}
+
+int getaddrinfo(const char *node, const char *service, const struct addrinfo *hints, struct addrinfo **res)
+{
+  int (*orig_getaddrinfo)(const char*, const char*, const struct addrinfo*, struct addrinfo**) = dlsym(RTLD_NEXT, "getaddrinfo");
+  int (*orig_freeaddrinfo)(struct addrinfo*) = dlsym(RTLD_NEXT, "freeaddrinfo");
+  struct dst dst;
+  struct addrinfo orighints = {.ai_family = AF_INET};
+  struct addrinfo *origres = NULL;
+  uint16_t port;
+  orig_getaddrinfo("localhost", service, &orighints, &origres);
+  port = ntohs(((struct sockaddr_in*)origres->ai_addr)->sin_port);
+  orig_freeaddrinfo(origres);
+  if (hints != NULL && hints->ai_family == AF_INET6)
+  {
+    struct sockaddr_in6 *sin6;
+    if (get_dst(&dst, 1, (char*)node) != 0)
+    {
+      return EAI_ADDRFAMILY;
+    }
+    if (dst.family != AF_INET6)
+    {
+      return EAI_ADDRFAMILY;
+    }
+    *res = malloc(sizeof(**res));
+    memset(*res, 0, sizeof(**res));
+    (*res)->ai_socktype = hints->ai_socktype;
+    (*res)->ai_protocol = hints->ai_protocol;
+    (*res)->ai_family = AF_INET6;
+    (*res)->ai_addrlen = sizeof(struct sockaddr_in6);
+    sin6 = malloc(sizeof(struct sockaddr_in6));
+    (*res)->ai_addr = (struct sockaddr*)sin6;
+    memset(sin6, 0, sizeof(struct sockaddr_in6));
+    sin6->sin6_family = AF_INET6;
+    sin6->sin6_port = htons(port);
+    memcpy(sin6->sin6_addr.s6_addr, dst.u.ipv6, 16);
+    return 0;
+  }
+  if (hints != NULL && hints->ai_family == AF_INET)
+  {
+    struct sockaddr_in *sin;
+    if (get_dst(&dst, 0, (char*)node) != 0)
+    {
+      return EAI_ADDRFAMILY;
+    }
+    if (dst.family != AF_INET)
+    {
+      return EAI_ADDRFAMILY;
+    }
+    *res = malloc(sizeof(**res));
+    memset(*res, 0, sizeof(**res));
+    (*res)->ai_socktype = hints->ai_socktype;
+    (*res)->ai_protocol = hints->ai_protocol;
+    (*res)->ai_family = AF_INET;
+    (*res)->ai_addrlen = sizeof(struct sockaddr_in);
+    sin = malloc(sizeof(struct sockaddr_in));
+    (*res)->ai_addr = (struct sockaddr*)sin;
+    memset(sin, 0, sizeof(struct sockaddr_in));
+    sin->sin_family = AF_INET;
+    sin->sin_port = htons(port);
+    if (dst.path[0] != '\0')
+    {
+      resolvs[cur_resolv] = dst;
+      sin->sin_addr.s_addr = htonl(cur_resolv);
+      cur_resolv++;
+      cur_resolv %= MAX_RESOLVS;
+    }
+    else
+    {
+      sin->sin_addr.s_addr = htonl(dst.u.ip);
+    }
+    return 0;
+  }
+  if (get_dst(&dst, 0, (char*)node) != 0)
+  {
+    return EAI_ADDRFAMILY;
+  }
+  if (dst.family == AF_INET)
+  {
+    struct sockaddr_in *sin;
+    *res = malloc(sizeof(**res));
+    memset(*res, 0, sizeof(**res));
+    if (hints)
+    {
+      (*res)->ai_socktype = hints->ai_socktype;
+      (*res)->ai_protocol = hints->ai_protocol;
+    }
+    (*res)->ai_family = AF_INET;
+    (*res)->ai_addrlen = sizeof(struct sockaddr_in);
+    sin = malloc(sizeof(struct sockaddr_in));
+    (*res)->ai_addr = (struct sockaddr*)sin;
+    memset(sin, 0, sizeof(struct sockaddr_in));
+    sin->sin_family = AF_INET;
+    sin->sin_port = htons(port);
+    if (dst.path[0] != '\0')
+    {
+      resolvs[cur_resolv] = dst;
+      sin->sin_addr.s_addr = htonl(cur_resolv);
+      cur_resolv++;
+      cur_resolv %= MAX_RESOLVS;
+    }
+    else
+    {
+      sin->sin_addr.s_addr = htonl(dst.u.ip);
+    }
+    return 0;
+  }
+  else if (dst.family == AF_INET6)
+  {
+    struct sockaddr_in6 *sin6;
+    *res = malloc(sizeof(**res));
+    if (hints)
+    {
+      (*res)->ai_socktype = hints->ai_socktype;
+      (*res)->ai_protocol = hints->ai_protocol;
+    }
+    memset(*res, 0, sizeof(**res));
+    (*res)->ai_family = AF_INET6;
+    (*res)->ai_addrlen = sizeof(struct sockaddr_in6);
+    sin6 = malloc(sizeof(struct sockaddr_in6));
+    (*res)->ai_addr = (struct sockaddr*)sin6;
+    memset(sin6, 0, sizeof(struct sockaddr_in6));
+    sin6->sin6_family = AF_INET6;
+    sin6->sin6_port = htons(port);
+    memcpy(sin6->sin6_addr.s6_addr, dst.u.ipv6, 16);
+    return 0;
+  }
+  else
+  {
+    abort();
+  }
+}
+
 struct hostent *gethostbyname(const char *name)
 {
   static struct hostent res;
